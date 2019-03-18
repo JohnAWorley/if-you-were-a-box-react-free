863f2c7f2b547a4ec35d12ef671c581bc9b1780e
John Worley <john.aver.worley@gmail.com>
2019-03-18 17:35:38 -0500

    box positions console logged

--- Server/scripts/infoScript.js
+++ Server/scripts/infoScript.js
@@ -1,7 +1,38 @@
 $(document).ready(readyNow);
 
+
 function readyNow() {
-    console.log('info script is up');
+    console.log('jquery ready');
+    $('#frameButton').on('click', getFaceCordinates);
 }
 
 
+function getFaceCordinates() {
+    console.log('in frame getter');
+    positionLoop();
+}
+
+
+function positionLoop() {
+    if (ctrack.getCurrentPosition()) {
+        var positions = ctrack.getCurrentPosition();
+        console.log(positions);
+        
+    }
+    else{
+        POSITIONS= null;
+    }
+}
+
+
+
+
+// function positionLoop() {
+//     console.log('in poistion loop');
+//     var frames = requestAnimationFrame(positionLoop);
+//     var positions = ctrack.getCurrentPosition();
+
+//     console.log(positions);
+
+
+// }
